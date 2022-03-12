9ae4a6420fd87309a9bd700a4e77367a35ab3226
Software.Goat <61810640+softwaregoat@users.noreply.github.com>
2022-03-13 00:55:49 +0800

    Office EWS test

---
+++ DotNetTest/DotNetTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExchangeService _service;
+
+            try
+            {
+                Console.WriteLine("Registering Exchange connection");
+
+                _service = new ExchangeService
+                {
+                    Credentials = new WebCredentials("software.goat@hotmail.com", "irontiger1125")
+                };
+            }
+            catch
+            {
+                Console.WriteLine("new ExchangeService failed. Press enter to exit:");
+                return;
+            }
+
+            // This is the office365 webservice URL
+            _service.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+
+            // Prepare seperate class for writing email to the database
+            try
+            {
+
+                Console.WriteLine("Reading mail");
+
+                // Read 100 mails
+                foreach (EmailMessage email in _service.FindItems(WellKnownFolderName.Inbox, new ItemView(100)))
+                {
+                    Console.WriteLine(email.From.Address);
+                }
+                Console.WriteLine("Exiting");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error has occured. \n:" + e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
